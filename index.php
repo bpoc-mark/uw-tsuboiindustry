8fdcb9812a6f7916f5a405ac800ae5c0240ab4c5
bpoc-mark <markariel.maata@gmail.com>
2021-12-06 18:03:04 +0800

    update on load script

--- index.php
+++ index.php
@@ -348,11 +348,19 @@
 	</script>
 
 	<script>
-		$(document).ready(function(){
-			// $(window).on('load', function (e) {
-				$('.sect_1 .wrapper').addClass('isOpened');
-			// })
+		// $(document).ready(function(){
+		// 	// $(window).on('load', function (e) {
+		// 		$('.sect_1 .wrapper').addClass('isOpened');
+		// 	// })
+		// });
+
+		const sect1 = document.querySelector('.sect_1 .wrapper');
+		window.addEventListener('load', function() {
+			sect1.classList.add('isOpened');
 		});
+		setTimeout( function () {
+			sect1.classList.add('isOpened');
+		}, 3000);
 	</script>
 </body>
 
