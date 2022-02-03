cdfe4e07c78d1c62c9b83c75793719ff95aa18d2
bpoc-mark <markariel.maata@bpoc.co.jp>
2022-02-03 17:55:45 +0800

    added qualified people page pc and sp view

--- inc/company-card-footer.php
+++ inc/company-card-footer.php
@@ -24,7 +24,7 @@ $str = explode('/', $url); ?>
             </a>
         </li>
         <li class="c-cardbtn--item">
-            <a href="" class="c-cardbtn--link">有資格者
+            <a href="/qualified-person/" class="c-cardbtn--link <?php if ($str[1] == "qualified-person") { echo ' c-cardbtn--link__active '; } ?>">有資格者
                 <span class="c-cardbtn--arrow"><img src="/images/common/arrow_icon.svg" alt=""></span>
             </a>
         </li>
